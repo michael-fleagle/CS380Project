58dbf25a6f599c1784df3dea468de7a26b53336e
Jacob R <Jacob.Rogers2@cwu.edu>
2022-04-21 15:32:53 -0700

    commit on 'main'

--- imageClassifier.py
+++ imageClassifier.py
@@ -1,9 +1,9 @@
 # Authors: Michael Fleagle, Kyah Fukunaga, Roman Kalabovda, Jacob Rogers
 
-#This is an initial test file for testing the gitLive code sharing application
+# This is an initial test file for testing the gitLive code sharing application
 
 import numpy as np
 import tensorflow
 
 
-#This works, I have finally gotten everything to work with anaconda using the method described by Kyah
+# This works, I have finally gotten everything to work with anaconda using the method described by Kyah
