1d593c3bb9dbd1425f5a1039ffc0f59356355238
Jacob R <Jacob.Rogers2@cwu.edu>
2022-04-25 13:33:37 -0700

    commit on 'main'

--- imageClassifier.py
+++ imageClassifier.py
@@ -3,7 +3,7 @@
 # This is an initial test file for testing the gitLive code sharing application
 
 import numpy as np
-import tensorflow
+import tensorflow as tf
 
 
 # This works, I have finally gotten everything to work with anaconda using the method described by Kyah
