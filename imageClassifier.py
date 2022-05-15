83c1ffdb3765b3b5424b4486571cf64da8e506ee
Jacob R <Jacob.Rogers2@cwu.edu>
2022-05-15 14:41:34 -0700

    commit on 'main'

--- imageClassifier.py
+++ imageClassifier.py
@@ -4,7 +4,6 @@
 
 import numpy as np
 import tensorflow as tf
-# from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
